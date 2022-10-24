b7748481a9a4fa019d06851b82673bc7934f1b01
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-10-24 12:27:05 +0100

    Create shortcuts.py

---
+++ shortcuts.py
@@ -0,0 +1,14 @@
+# shortcuts
+
+# CRTL + P - parameter info
+# CTRL + space - autofill/completions
+# CTRL + Q  - documentation
+# SHIFT + ALT + E - execute code
+# CTRL + S - save
+# Ctrl+Alt+Shift+L - format code
+# Ctrl + Shift + n fine file 
+# shift + shift - find files/classes 
+# CTRL + Shift + T creates a test of a function 
+
+# Tools > sync python requirements 
+# File > settings > project interpreter 
