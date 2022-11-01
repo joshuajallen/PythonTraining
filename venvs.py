8573713d5a9a9a3907abb0c24793ff3a4ad35cad
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-11-01 09:42:42 +0000

    Update venvs.py

--- venvs.py
+++ venvs.py
@@ -1,3 +1,9 @@
 # !conda --version
 # !conda activate quantlib_bbg
 # !conda info --envs
+
+
+# command line: 
+
+# activate quantlib_bbg
+# N:/Offdata/RM/_People/JA/Python/packages/QuantLib-1.18-cp36-cp36m-win_amd64.whl
