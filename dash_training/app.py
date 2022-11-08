83733497e16b6c72de395a216422bd540eab9c03
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-11-08 18:03:38 +0000

    Update app.py

--- dash_training/app.py
+++ dash_training/app.py
@@ -2,15 +2,11 @@
 # https://dash-bootstrap-components.opensource.faculty.ai/docs/components/input/
 
 # load packages
-
-import os
 import dash
-from dash import html
-from dash import dcc
 from dash.dependencies import Output, Input
 import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
 import plotly.express as px
-import pandas as pd
 import webbrowser
 from threading import Timer
 
@@ -18,22 +14,37 @@ from threading import Timer
 # exec(open("./layout/page_one.py").read())
 from layout.page_one import page_one, data
 from layout.page_two import page_two
+from layout.page_three import page_three
+from layout.page_four import page_four
+from layout.global_variables import *
 
+load_figure_template('LUX')
 
 tabs_styles = {'display': 'inlineBlock'}
-icons = ["fa-globe", "fa-chart-area", "fa-chart-area", "fa-chart-area"]
-titles = ["Asset prices", "Portfolio Properties", "Tab 3 placer", "Tab 4 placer"] #html.I("Asset prices", className="fas fa-shield")
-contents = [page_one, page_two, html.Div(), html.Div()]
+
+icons = ["fas fa-globe",
+         "fas fa-chart-area",
+         "fas fa-box",
+         "fas fa-chart-area"]
+
+titles = ["Asset prices",
+          "Portfolio Properties",
+          "Data Tables",
+          "Tab 4 placer"]
+
+contents = [page_one, page_two, page_three, page_four]
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 app = dash.Dash(__name__,
                 external_stylesheets=[dbc.themes.LUMEN],
                 suppress_callback_exceptions=True,
                 assets_folder='N:\\Offdata\\RM\\_People\\JA\\Python\\dash_intro\\assets')
 
-app.title = "Asset price dashboard"
+app.title = "RM dashboard"
 
 port = 1500  # or simply open on the default `8050` port
+
 
 def open_browser():
     webbrowser.open_new("http://localhost:{}".format(port))
@@ -41,19 +52,20 @@ def open_browser():
 
 def make_a_tab(icon, title, content):
     return dcc.Tab(
-        label=title, className=icon + " custom-tab",
-        selected_className="custom-tab--selected",
+        label=title, className=icon,
         value=title.lower().replace(" ", "-"),
         children=[
+            html.Br(),
+            html.Hr(),
             html.Br(),
             # html.H3(title),
             # html.P(f"This is the tab {title}."),
             content
         ])
 
-# app.layout = html.P("Hello world")  # p is a paragraph
-# define layout using html div
+
 # this section is essentially the UI, populating inputs/outputs/headings/containers etc.
+
 app.layout = dbc.Container(
     fluid=True,
     children=[
@@ -75,7 +87,7 @@ app.layout = dbc.Container(
                     children=[
                         html.H1(
                             id="header-title",
-                            children="Precious Metal Prices",
+                            children="RM dashboard",
                         ),
                         html.Br(),
                         html.P(
@@ -96,20 +108,12 @@ app.layout = dbc.Container(
                 children=[make_a_tab(icon, title, content) for icon, title, content, in zip(icons, titles, contents)],
                 value="Asset prices")
 
-# dbc.Tabs(id="tabs",
-#          style=tabs_styles,
-#          className='custom-tabs-container',
-#          active_tab="Asset prices",
-#          children=[
-#              # section for tabs
-#              page_one,
-#              page_two
-#          ])
-])
-]
+        ])
+    ]
 )
 
 server = app.server
+
 
 @app.callback(
     Output("price-chart", "figure"),
@@ -137,19 +141,64 @@ def update_chart(metal, start_date, end_date):
     )
 
     fig.update_layout(
-        template="ggplot2",
         xaxis_title="Date",
         yaxis_title="Price (USD/oz)",
         font=dict(
             family="Verdana, sans-serif",
-            size=14
+            size=10
         ),
     )
 
     return fig
 
 
+@app.callback(
+    Output("scatter-chart", "figure"),
+    Input("date_range", "start_date"),
+    Input("date_range", "end_date")
+)
+def update_scatter(start_date, end_date):
+    # Create a plotly plot for use by dcc.Graph().
+    plot_data=data.loc[(data.DateTime >= start_date) & (data.DateTime <= end_date)]
+    dist=px.scatter_matrix(plot_data,
+                         dimensions=plot_data.columns[1:],
+                         title="Scatter matrix:")
+
+    dist.update_layout(
+        font=dict(
+            family="Verdana, sans-serif",
+            size=12
+        ),
+    )
+
+    return dist
+
+
+@app.callback(
+    Output("page_two_scatter", "figure"),
+    Input("metal_one", "value"),
+    Input("metal_two", "value")
+)
+def update_page_two(metal_one, metal_two):
+    # Create a plotly plot for use by dcc.Graph().
+    plot_data=data
+    scatter=px.scatter(plot_data,
+                       x=data[metal_one],
+                       y=data[metal_two],
+                       color_discrete_sequence=COLOUR_PALETTE)
+
+    scatter.update_layout(
+        font=dict(
+            family="Verdana, sans-serif",
+            size=12
+        ),
+    )
+
+    return scatter
+
+
 if __name__ == '__main__':
     Timer(1, open_browser).start();
     app.run_server(debug=False, port=port)
+
 
