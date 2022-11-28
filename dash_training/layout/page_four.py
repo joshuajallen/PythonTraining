8b98e3e814555b617b9d3443ae2a8ce24358b84d
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-11-28 16:51:48 +0000

    Update page_four.py

--- dash_training/layout/page_four.py
+++ dash_training/layout/page_four.py
@@ -1,30 +1,107 @@
-from dash import Dash, dash_table, dcc, html
+from dash import dash_table
+from dash import Input, Output, State, html
 import dash_bootstrap_components as dbc
 from layout.global_variables import *
 import pandas as pd
+import numpy as np
+
+pd.options.display.float_format = '${:.2f}'.format
 
 df = pd.melt(data, id_vars='DateTime')
 df = df.groupby('variable').agg(
     {
-        "value": ["min", "max", "median", "skew"]
+        "value": ["min", "max", "median", "skew", np.std]
     }
 )
 df = df.droplevel(0, axis=1).reset_index(level=0)
-cols = ["min", "max", "median", "skew"]
+df = df.round(2)
+cols = ["min", "max", "median", "skew", "std"]
 
-cols = ["min", "max", "median", "skew"]
 (styles, legend) = discrete_background_color_bins(df, columns = cols)
 
-page_four = dbc.Tab(label='Data Tables',
-                     className="fas fa-box",
-                     children=[
-                         html.Div([
-                             dash_table.DataTable(
-                                 id='table',
-                                 columns=[{"name": i, "id": i} for i in df.columns],
-                                 data=df.to_dict('records'),
-                                 style_data_conditional=styles
-                             ),
 
-                         ])
-                     ])
+summary = pd.melt(data, id_vars='DateTime')
+summary = summary.groupby('variable').agg(
+    {
+        "value": ["min", "max", "median", "skew", np.std]
+    }
+)
+
+summary = summary.droplevel(0, axis=1).reset_index(level=0)
+cols = ["variable", "min", "max", "median", "skew", "std"]
+summary.columns = cols
+
+gold = summary[summary['variable'] == 'Gold']['std']
+silver = summary[summary['variable'] == 'Silver']['std']
+# df.loc[df['variable'] == 'Gold', 'std']
+
+modal = html.Div(
+    [
+        dbc.Button("Open modal", id="open", n_clicks=0),
+        dbc.Modal(
+            [
+                dbc.ModalHeader("Header"),
+                dbc.ModalBody(html.Div([
+                    dash_table.DataTable(
+                        id='modal_table',
+                        columns=[{"name": i, "id": i} for i in df.columns],
+                        data=df.astype(str).to_dict('records'),
+                        style_data_conditional=styles
+                    ),
+                ])),
+                dbc.ModalFooter(
+                    dbc.Button(
+                        "Close", id="close", className="ms-auto", n_clicks=0
+                    )
+                ),
+            ],
+            id="modal",
+            is_open=False,
+        ),
+    ]
+)
+
+
+page_four = dbc.Tab(label='Data Report 🗓️',
+                    className="fas fa-box",
+                    children=[
+                        dbc.Row([
+                            dbc.Col(modal, md=4),
+                            dbc.Col([
+                                dbc.Alert(
+                                    ["Gold sd: ", html.A(gold.values.round(1), href="Tab1", className="alert-link")],
+                                    color="info",
+                                )], md=2),
+                            dbc.Col([
+                                dbc.Alert(
+                                    ["Silver: ", html.A(silver.values.round(1), href="#", className="alert-link")],
+                                    color="info",
+                                )], md=2),
+                            dbc.Col([
+                                dbc.Alert(
+                                    "info",
+                                    color="#8B0000",
+                                )], md=2),
+                            dbc.Col([
+                                dbc.Alert(
+                                    html.A("primary color", href="#", className="alert-link"),
+                                    color="primary",
+                                )], md=2)
+                        ]),
+                        html.Br(),
+                        html.Hr(),
+                        html.Br(),
+                        dbc.Row([
+                            dbc.Col([
+                            html.Div([
+                                dash_table.DataTable(
+                                    id='table',
+                                    columns=[{"name": i, "id": i} for i in df.columns],
+                                    data=df.astype(str).to_dict('records'),
+                                    style_data_conditional=styles
+                                ),
+                            ])
+                                ], md = 12)
+                        ])
+
+                    ])
