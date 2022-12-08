aa9d39d99e569e0fb6104d901bcce795e4031722
joshuajallen <103208650+joshuajallen@users.noreply.github.com>
2022-12-08 16:05:46 +0000

    Create vscode.py

---
+++ vscode.py
@@ -0,0 +1,9 @@
+# C:\Miniconda3\python.exe - select enviroment 
+
+# settings 
+
+# Preferences: Open Settings (JSON) 
+# Python: Select Interpreter 
+# Prerferences: Open Workspace Settings
+# Python: Run File in Termnal 
+# Jupyter: Create Interactive Window 
